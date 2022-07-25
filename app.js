62cc71edeeab04f3c20a7f708e27ea929c9ab5c3
yamamo0301 <101253886+yamamo0301@users.noreply.github.com>
2022-07-25 08:55:58 +0000

    [Finish]JS_1

---
+++ app.js
@@ -0,0 +1,10 @@
+let orange = 120;
+let apple = 100;
+
+if(orange < apple){
+  alert('みかんの値段がりんごより安い');
+} else if(orange == apple){
+  alert('みかんとりんごが同じ値段');
+} else{
+  alert('みかんの値段がりんごより高い');
+}

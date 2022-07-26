aa2d7a8265528fba5c2b041fb6595a2c2f59851b
yamamo0301 <101253886+yamamo0301@users.noreply.github.com>
2022-07-26 15:06:48 +0000

    [Finish]JS_3

---
+++ function.js
@@ -0,0 +1,12 @@
+// メイン部分
+let alertString;
+alertString = addString("WebCamp");
+
+// 作成した関数を呼び出す
+alert(alertString);
+
+// 作成した関数
+function addString(strA) {
+  let addStr = "Hello" + strA;
+  return addStr;
+}
